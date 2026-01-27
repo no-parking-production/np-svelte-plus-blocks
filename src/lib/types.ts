9cf528ca6fb27b13d6e256ff79b670b64302aa7d
Samuel Walz <sw@noparking.dk>
2026-01-27 14:05:38 +0100

    Update `ButtonProps` to make `secondaryActions` optional, export `BAction`, and bump version to 0.1.3

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,10 @@
-import type {ButtonAction} from "$lib/common/button/ButtonAction.js";
+import type { ButtonAction } from "$lib/common/button/ButtonAction.js";
+import { BAction } from "$lib/common/button/BAction.js";
 
+export { BAction };
 export interface ButtonProps {
     label: string;
     primaryAction: ButtonAction;
-    secondaryActions: ButtonAction[];
+    secondaryActions?: ButtonAction[];
 }
  
