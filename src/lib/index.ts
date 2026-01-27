f2c0241b22a942b4170f5b49ed4724b1742e3dc0
Samuel Walz <sw@noparking.dk>
2026-01-27 14:16:18 +0100

    Export `BlocksInterface` in index.ts and bump version to 0.1.5.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,6 @@ export { default as BlocksButton } from './components/BlocksButton.svelte';
 
 // export common
 export { BAction } from './common/button/BAction.ts';
+export { BlocksInterface } from './common/BlocksInterface.ts';
 
 export type * from './types.js';
