8586003348228c36925c814e40e1a1a32a2336e1
wasam_np <sw@noparking.dk>
2026-06-04 11:25:42 +0200

    Add test page with BlocksButton examples for parameter manipulation

---
+++ src/routes/test-a/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+
+    import {BlocksInterface} from "$lib/common/BlocksInterface.js";
+    import {BAction} from "$lib/common/button/BAction.js";
+    import BlocksButton from "$lib/components/BlocksButton.svelte";
+
+
+    const blocks = BlocksInterface.getInstance();
+    blocks?.addTags('hey');
+    blocks?.gotoBlock('test');
+
+    let test = blocks?.getParamStore('test');
+    let num = blocks?.getParamStore('num');
+</script>
+
+
+<BlocksButton label="local test on"
+              primaryAction={BAction.SetProperty('test', 'lala')}
+/>
+<BlocksButton label="local test off"
+              primaryAction={BAction.SetProperty('test', false)}
+/>
+
+
+
+&nbsp;
+<BlocksButton label="realm test on"
+              primaryAction={BAction.SetProperty('Realm.Test_realm.variable.test_bool', true)}
+/>
+<BlocksButton label="realm test off"
+              primaryAction={BAction.SetProperty('Realm.Test_realm.variable.test_bool', false)}
+/>
+
+<div>{$test} {$num}</div>
+
+
+
+
